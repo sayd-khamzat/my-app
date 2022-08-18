622826a4d07b684737ed17dff89a70fadd243a11
sayd-khamzat <111534427+sayd-khamzat@users.noreply.github.com>
2022-08-19 00:06:04 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <header className='header'>
+                <img src="https://i.pinimg.com/736x/58/ba/ce/58bace42e0e777c45691cecfc1f4167e.jpg"/>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div>
+            <div className='content'>
+                <div>
+                    <img
+                        src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"/>
+                </div>
+                <div>
+                    ava + description
+                    <img src="https://udoba.org/sites/default/files/h5p/content/21522/images/file-6113d5f8845dc.jpeg"/>
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        Posts
+                        <div>
+                            Post1
+                        </div>
+                        <div>
+                            Post2
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default Profile;
